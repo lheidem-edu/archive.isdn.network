ca8873993cbf366e254298bc8fcb5f0d447ecc1f
Luca Heidemann <kontakt@lheidem.net>
2025-06-01 19:51:38 +0200

    Truncate artifact file name for mobile devices

--- src/app/artifacts/[id]/page.tsx
+++ src/app/artifacts/[id]/page.tsx
@@ -31,7 +31,7 @@ export default async function ArtifactDetailPage({ params }: { params: Promise<{
                 <h1 className="text-3xl font-extrabold tracking-tight text-neutral-200">
                     Artifact Details
                 </h1>
-                <p className="text-neutral-400">
+                <p className="text-neutral-400 truncate">
                     {artifact.artifact_file_name} ({artifact.artifact_file_length} bytes)
                 </p>
                 <p className="text-neutral-400">
